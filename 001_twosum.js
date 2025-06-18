5ec8968ec28d19aaf273fbb187ba22efbb0b996f
june9999 <vesper0@163.com>
2025-06-18 13:06:29 +0200

    add init

---
+++ 001_twosum.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  // brute force
+  // let index = 0
+  // for (index = 0; index < nums.length;index++) {
+  //     let searchValue = target - nums[index]
+  //     for (let i = index + 1; i < nums.length; i++) {
+  //         if (searchValue === nums[i]) {
+  //             return [index,i]
+  //         }
+  //     }
+  // }
+  let hashtable = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (hashtable.has(target - nums[i])) {
+      return [hashtable.get(target - nums[i]), i];
+    }
+    hashtable.set(nums[i], i);
+  }
+  return [];
+};
